0b35139869371a4b986adccb2e4954832aeca2a3
alvcodec <148978672+Alvicodes@users.noreply.github.com>
2024-11-26 19:42:52 +0000

    Dashboard update

--- admin-dashboard/src/App.js
+++ admin-dashboard/src/App.js
@@ -7,14 +7,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
 import "./App.css";
-import Header from "./components/Header";
-import SideBar from "./components/SideBar";
+import Header from "./components/header/Header";
+import SideBar from "./components/sidebar/SideBar";
+import Main from "./components/main/Main";
 
 function App() {
   return (
     <>
       <Header />;
       <SideBar />
+      <Main />
     </>
   );
 }
