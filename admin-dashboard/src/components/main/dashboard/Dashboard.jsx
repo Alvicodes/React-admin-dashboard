421c56b7d8326c56e539afdf9b53bc6549ca41ae
alvcodec <148978672+Alvicodes@users.noreply.github.com>
2025-03-09 19:06:12 +0000

    Dashboard update

--- admin-dashboard/src/components/main/dashboard/Dashboard.jsx
+++ admin-dashboard/src/components/main/dashboard/Dashboard.jsx
@@ -4,6 +4,8 @@ import Card from './Card';
 import Reports from './report/Reports';
 import RecentSales from './RecentSales';
 import TopSelling from './TopSelling';
+import RecentActivity from './recent-activity/RecentActivity';
+import BudgetReport from './budgetReport/BudgetReport';
 
 function Dashboard() {
     const [cards, setCards] = useState([])
@@ -41,7 +43,11 @@ function Dashboard() {
                         </div>
                     </div>
                 </div>
-                <div className="col-lg-4"></div>
+                <div className="col-lg-4">
+                    <RecentActivity />
+                    <BudgetReport />
+
+                </div>
             </div>
         </section>
     );
