b13110e7e3454230344bdafd87c24b7aff3771df
alvcodec <148978672+Alvicodes@users.noreply.github.com>
2025-03-09 19:06:46 +0000

    Budget  section in dashboard

---
+++ admin-dashboard/src/components/main/dashboard/budgetReport/BudgetChart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as echarts from 'echarts';
+
+function BudgetChart() {
+    return (
+        <div>
+            BudgetChart
+        </div>
+    )
+}
+
+export default BudgetChart;
